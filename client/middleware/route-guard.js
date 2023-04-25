c33a2fe15551c7514b8619b226bd301d7854a87e
kevz <55552649+kennael22@users.noreply.github.com>
2023-04-25 14:43:43 +0800

    Create route-guard.js

---
+++ client/middleware/route-guard.js
@@ -0,0 +1,7 @@
+export default function(context) {
+    if (context.route.meta.length) {
+        if (!!context.route.meta[0].routeAccess && !window.$nuxt.$can(context.route.meta[0].routeAccess)) {
+            window.$nuxt.error({ statusCode: 404 })
+        }
+    }
+}
